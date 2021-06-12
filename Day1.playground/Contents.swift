7d3975a61710f4588a56350b7517b55a688f36ba
Marcelo Pecin <celopeccin@gmail.com>
2021-06-12 01:35:11 -0300

    feat: Day 1 - variables, simple data types, and string interpolation

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,63 @@
+import UIKit
+
+// Variables
+var greeting = "Hello, playground"
+greeting = "Goodbye!"
+
+var operatingSystem = "macOS"
+
+// String and Integers
+var age = 26
+var population = 8_000_000
+
+var meaningOfLife = 42
+//meaningOfLife = "Forty two" (error)
+
+var highScore = 328_556
+
+// Multi-line strings
+var multiLines = """
+This goes
+over multiple
+lines
+"""
+
+var multiLinesFormat = """
+This goes \
+over multiple \
+lines
+"""
+
+// Doubles and booleans
+var pi = 3.141
+
+var awesome = true
+var horrible = false
+
+var myInt = 1
+var myDouble = 1.0
+//var total = myInt + myDouble (error)
+
+var loggedIn = true
+
+// String interpolation
+var score = 19
+var str = "You score was \(score)"
+
+var results = "The test results are here:  \(str)"
+
+// Constants
+let taylor = "swift"
+//taylor = "flutter" (error)
+
+let enabled = true
+
+let myAge = 26
+
+// Type annotations
+let album: String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let marceloRocks: Bool = true
+
+var sales: Double = 100_000
